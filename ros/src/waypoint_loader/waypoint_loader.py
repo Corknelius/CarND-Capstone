329b5bd5e9a69c4d8c1606f79a52a307603e5339
zhouwubai <zhouwubai@gmail.com>
2018-05-28 15:34:43 -0700

    add data type docstr

--- ros/src/waypoint_loader/waypoint_loader.py
+++ ros/src/waypoint_loader/waypoint_loader.py
@@ -16,6 +16,45 @@ MAX_DECEL = 1.0
 
 
 class WaypointLoader(object):
+    """
+    Type Waypoint consists of (post, twist)
+
+    geometry_msgs/PoseStamped pose
+    geometry_msgs/TwistStamped twist
+
+    Type Info:
+
+    geometry_msgs/PoseStamped
+    std_msgs/Header header
+        uint32 seq
+        time stamp
+        string frame_id
+    geometry_msgs/Pose pose
+        geometry_msgs/Point position
+            float64 x
+            float64 y
+            float64 z
+        geometry_msgs/Quaternion orientation
+            float64 x
+            float64 y
+            float64 z
+            float64 w
+
+    geometry_msgs/TwistStamped
+    std_msgs/Header header
+        uint32 seq
+        time stamp
+        string frame_id
+    geometry_msgs/Twist twist
+        geometry_msgs/Vector3 linear
+            float64 x
+            float64 y
+            float64 z
+        geometry_msgs/Vector3 angular
+            float64 x
+            float64 y
+            float64 z
+    """
 
     def __init__(self):
         rospy.init_node('waypoint_loader', log_level=rospy.DEBUG)
@@ -24,12 +63,14 @@ class WaypointLoader(object):
                                    queue_size=1, latch=True)
 
         self.velocity = self.kmph2mps(rospy.get_param('~velocity'))
+        # file data/wp_yaw_const.csv
         self.new_waypoint_loader(rospy.get_param('~path'))
         rospy.spin()
 
     def new_waypoint_loader(self, path):
         if os.path.isfile(path):
             waypoints = self.load_waypoints(path)
+            # one time publish
             self.publish(waypoints)
             rospy.loginfo('Waypoint Loded')
         else:
@@ -59,13 +100,14 @@ class WaypointLoader(object):
 
     def distance(self, p1, p2):
         x, y, z = p1.x - p2.x, p1.y - p2.y, p1.z - p2.z
-        return math.sqrt(x*x + y*y + z*z)
+        return math.sqrt(x * x + y * y + z * z)
 
     def decelerate(self, waypoints):
         last = waypoints[-1]
         last.twist.twist.linear.x = 0.
         for wp in waypoints[:-1][::-1]:
-            dist = self.distance(wp.pose.pose.position, last.pose.pose.position)
+            dist = self.distance(wp.pose.pose.position,
+                                 last.pose.pose.position)
             vel = math.sqrt(2 * MAX_DECEL * dist)
             if vel < 1.:
                 vel = 0.
