6c6a3d5c957f43a280ccd06780aec10bd37c6f6b
zhouwubai <zhouwubai@gmail.com>
2018-05-30 22:17:11 -0700

    fix some codestyle

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -2,32 +2,38 @@
 
 import rospy
 from std_msgs.msg import Bool
-from dbw_mkz_msgs.msg import ThrottleCmd, SteeringCmd, BrakeCmd, SteeringReport
+from dbw_mkz_msgs.msg import (
+    ThrottleCmd, SteeringCmd,
+    BrakeCmd, SteeringReport
+)
 from geometry_msgs.msg import TwistStamped
 import math
 
 from twist_controller import Controller
 
 '''
-You can build this node only after you have built (or partially built) the `waypoint_updater` node.
+You can build this node only after you have built (or partially built) the
+`waypoint_updater` node.
 
-You will subscribe to `/twist_cmd` message which provides the proposed linear and angular velocities.
-You can subscribe to any other message that you find important or refer to the document for list
-of messages subscribed to by the reference implementation of this node.
+You will subscribe to `/twist_cmd` message which provides the proposed linear
+and angular velocities. You can subscribe to any other message that you find
+important or refer to the document for list of messages subscribed to by the
+reference implementation of this node.
 
-One thing to keep in mind while building this node and the `twist_controller` class is the status
-of `dbw_enabled`. While in the simulator, its enabled all the time, in the real car, that will
-not be the case. This may cause your PID controller to accumulate error because the car could
-temporarily be driven by a human instead of your controller.
+One thing to keep in mind while building this node and the `twist_controller`
+class is the status of `dbw_enabled`. While in the simulator, its enabled all
+the time, in the real car, that will not be the case. This may cause your PID
+controller to accumulate error because the car could temporarily be driven by
+a human instead of your controller.
 
-We have provided two launch files with this node. Vehicle specific values (like vehicle_mass,
-wheel_base) etc should not be altered in these files.
+We have provided two launch files with this node. Vehicle specific values
+(like vehicle_mass, wheel_base) etc should not be altered in these files.
 
-We have also provided some reference implementations for PID controller and other utility classes.
-You are free to use them or build your own.
+We have also provided some reference implementations for PID controller and
+other utility classes. You are free to use them or build your own.
 
-Once you have the proposed throttle, brake, and steer values, publish it on the various publishers
-that we have created in the `__init__` function.
+Once you have the proposed throttle, brake, and steer values, publish it on
+the various publishers that we have created in the `__init__` function.
 
 '''
 
@@ -61,7 +67,7 @@ class DBWNode(object):
         self.loop()
 
     def loop(self):
-        rate = rospy.Rate(50) # 50Hz
+        rate = rospy.Rate(50)
         while not rospy.is_shutdown():
             # TODO: Get predicted throttle, brake, and steering using `twist_controller`
             # You should only publish the control commands if dbw is enabled
