b155043bf4c2f71456a225b29128cfe8339c71e1
zhouwubai <zhouwubai@gmail.com>
2018-05-30 22:45:12 -0700

    add dbw_node walkthrough

--- ros/src/twist_controller/pid.py
+++ ros/src/twist_controller/pid.py
@@ -18,10 +18,10 @@ class PID(object):
 
     def step(self, error, sample_time):
 
-        integral = self.int_val + error * sample_time;
-        derivative = (error - self.last_error) / sample_time;
+        integral = self.int_val + error * sample_time
+        derivative = (error - self.last_error) / sample_time
 
-        val = self.kp * error + self.ki * integral + self.kd * derivative;
+        val = self.kp * error + self.ki * integral + self.kd * derivative
 
         if val > self.max:
             val = self.max
@@ -30,5 +30,8 @@ class PID(object):
         else:
             self.int_val = integral
         self.last_error = error
+
+        # rospy.logwarn("Throttle: {}".format(val))
+        # rospy.lowwarn("Velocity error: {0}".format(error))
 
         return val
