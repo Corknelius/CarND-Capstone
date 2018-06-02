4d81deff6a237ad08a3ef1baaa3c03a44b07c4b1
zhouwubai <zhouwubai@gmail.com>
2018-06-02 16:33:29 -0700

    fix styx code style

--- ros/src/styx/server.py
+++ ros/src/styx/server.py
@@ -17,16 +17,20 @@ msgs = []
 
 dbw_enable = False
 
+
 @sio.on('connect')
 def connect(sid, environ):
     print("connect ", sid)
 
+
 def send(topic, data):
     s = 1
     msgs.append((topic, data))
-    #sio.emit(topic, data=json.dumps(data), skip_sid=True)
+    # sio.emit(topic, data=json.dumps(data), skip_sid=True)
+
 
 bridge = Bridge(conf, send)
+
 
 @sio.on('telemetry')
 def telemetry(sid, data):
@@ -39,25 +43,31 @@ def telemetry(sid, data):
         topic, data = msgs.pop(0)
         sio.emit(topic, data=data, skip_sid=True)
 
+
 @sio.on('control')
 def control(sid, data):
     bridge.publish_controls(data)
+
 
 @sio.on('obstacle')
 def obstacle(sid, data):
     bridge.publish_obstacles(data)
 
+
 @sio.on('lidar')
 def obstacle(sid, data):
     bridge.publish_lidar(data)
+
 
 @sio.on('trafficlights')
 def trafficlights(sid, data):
     bridge.publish_traffic(data)
 
+
 @sio.on('image')
 def image(sid, data):
     bridge.publish_camera(data)
+
 
 if __name__ == '__main__':
 
